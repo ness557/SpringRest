d658faceb108006b4515045d1256dc94931bede0
ness <ness55779@gmail.com>
2018-05-21 10:12:51 +0300

    Moved REST "post (update) student" and "delete student" to spring-annotations

--- src/main/java/ness/controller/MyRestController.java
+++ src/main/java/ness/controller/MyRestController.java
@@ -6,16 +6,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.view.json.MappingJackson2JsonView;
 import ness.service.StudentService;
 
-//import javax.servlet.ServletRequest;
-//import javax.servlet.http.HttpServletRequest;
-//import javax.servlet.http.HttpServletResponse;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Map;
 
 @RestController
 @RequestMapping("/rest")
@@ -50,112 +44,20 @@ public class MyRestController {
         return service.addStudent(new Student(id, name, group));
     }
 
+    @RequestMapping(value = "students", method = RequestMethod.DELETE)
+    public int deleteStudent(@RequestParam(value = "id") int id){
 
-//    public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
-//
-//        ModelAndView mnv = null;
-//        Map<String, String[]> params = request.getParameterMap();
-//
-//        switch (request.getMethod()) {
-//
-//            case "GET":
-//
-//                mnv = new ModelAndView(new MappingJackson2JsonView());
-//                if (params.isEmpty()) {
-//
-//                    for (Student st : service.getStudents()) {
-//                        mnv.addObject("student" + st.getId(), st);
-//                    }
-//
-//                } else {
-//
-//                    Student st = null;
-//                    try {
-//                        st = service.getStudentById(
-//                          Integer.valueOf(
-//                                  request.getParameter("id")));
-//                    } catch (NumberFormatException e) {
-//                        e.printStackTrace();
-//                    }
-//                    if (st != null)
-//                        mnv.addObject(st);
-//                }
-//                break;
-//            case "PUT":
-//
-//                response.setContentType("text/plain");
-//
-//                if (params.containsKey("id") && params.containsKey("name") && params.containsKey("group")) {
-//
-//                    try {
-//
-//                        int id = Integer.valueOf(params.get("id")[0]);
-//                        String name = params.get("name")[0];
-//                        String group = params.get("group")[0];
-//
-//                        response.getWriter().write(
-//                                String.valueOf(
-//                                        service.addStudent(new Student(id, name, group))));
-//
-//                    } catch (Exception e) {
-//                        response.getWriter().write("0");
-//                        e.printStackTrace();
-//                    }
-//
-//                } else
-//                    response.getWriter().write("0");
-//
-//                break;
-//
-//
-//            case "DELETE":
-//                response.setContentType("text/plain");
-//
-//                if(params.containsKey("id")){
-//
-//                    try {
-//                        int id = Integer.valueOf(params.get("id")[0]);
-//                        response.getWriter().write(
-//                                String.valueOf(
-//                                        service.removeStudent(id)));
-//                    } catch (Exception e) {
-//                        e.printStackTrace();
-//                        response.getWriter().write("0");
-//                    }
-//
-//                }
-//                else
-//                    response.getWriter().write("0");
-//
-//                break;
-//            case "POST":
-//
-//                response.setContentType("text/plain");
-//
-//                if (params.containsKey("id") && params.containsKey("name") && params.containsKey("group")) {
-//
-//                    try {
-//
-//                        int id = Integer.valueOf(params.get("id")[0]);
-//                        String name = params.get("name")[0];
-//                        String group = params.get("group")[0];
-//
-//                        response.getWriter().write(
-//                                String.valueOf(
-//                                        service.updateStudent(new Student(id, name, group))));
-//
-//                    } catch (Exception e) {
-//                        response.getWriter().write("0");
-//                        e.printStackTrace();
-//                    }
-//
-//                } else
-//                    response.getWriter().write("0");
-//
-//                break;
-//        }
-//
-//        return mnv;
-//    }
+        return service.removeStudent(id);
+
+    }
+
+    @RequestMapping(value = "students", method = RequestMethod.POST)
+    public int updateStudent(@RequestParam(value = "id") int id,
+                             @RequestParam(value = "name") String name,
+                             @RequestParam(value = "group") String group){
+
+        return service.updateStudent(new Student(id, name, group));
+
+    }
 }
 
